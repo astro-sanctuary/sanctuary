7bc239b4177aa613839e702851711db120b1d3e2
Brian Perry <contact.brianperry@gmail.com>
2024-04-21 14:01:59 -0500

    Adapted blog list to use schema

--- templates/blog-ts/src/lib/client.ts
+++ templates/blog-ts/src/lib/client.ts
@@ -1,6 +1,3 @@
 import { JsonApiClient } from "@drupal-api-client/json-api-client";
-import { Jsona } from "jsona";
 
-export const client = new JsonApiClient(import.meta.env.DRUPAL_BASE_URL, {
-  serializer: new Jsona(),
-});
+export const client = new JsonApiClient(import.meta.env.DRUPAL_BASE_URL);
