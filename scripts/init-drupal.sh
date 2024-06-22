f4652bd307ccf9f81577440ab72717b531151583
Brian Perry <contact.brianperry@gmail.com>
2024-06-22 10:37:02 -0500

    Add decoupled_router to recipe

--- scripts/init-drupal.sh
+++ scripts/init-drupal.sh
@@ -12,7 +12,12 @@ ddev composer create drupal/recommended-project
 
 # Prevent composer scaffolding from overwriting development.services.yml
 ddev composer config --json extra.drupal-scaffold.file-mapping '{"[web-root]/sites/development.services.yml": false}'
-ddev composer config minimum-stability alpha
+# ddev composer config --json --merge extra.installer-paths '{"web/recipes/contrib/{$name}": ["type:drupal-recipe"]}'
+ddev composer config minimum-stability dev
+ddev composer config allow-plugins.ewcomposer/unpack true -n
+# Add repositories
+ddev composer config repositories.unpack vcs https://gitlab.ewdev.ca/yonas.legesse/drupal-recipe-unpack.git
+ddev composer config repositories.recipe path web/recipes/sanctuary_core
 
 # Open up CORS for local development
 # cat ../scripts/config/enable-local-settings.php >> web/sites/default/settings.php
@@ -21,15 +26,18 @@ ddev composer config minimum-stability alpha
 # mkdir config
 # cp ../scripts/config/graphql_compose.settings.yml config/graphql_compose.settings.yml
 
+# Add recipies
+cp -a ../recipes/. web/recipes
+
 # Add useful composer dependencies
-ddev composer require drush/drush drupal/devel
+ddev composer require drush/drush drupal/devel drupal/sanctuary_core ewcomposer/unpack:dev-master
 
 # Install Drupal
 ddev drush site:install --account-name=admin --account-pass=admin -y
 
 # Apply recipe
-cp -a ../recipes/. web/recipes
 ddev exec -d /var/www/html/web php core/scripts/drupal recipe recipes/sanctuary_core
+ddev composer unpack drupal/sanctuary_core
 
 ### Below should be handled by recipes ###
 
