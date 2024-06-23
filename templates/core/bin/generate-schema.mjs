941152663c7b19427767f47ff1b9f5b1395a5b99
Brian Perry <contact.brianperry@gmail.com>
2024-06-23 13:53:48 -0500

    Update schema script, use schema in about template

--- templates/core/bin/generate-schema.mjs
+++ templates/core/bin/generate-schema.mjs
@@ -1,10 +1,12 @@
 import * as fs from "fs";
-import { client } from "../src/lib/client.js";
+import { client } from "../client.js";
 import { jsonToZod } from "json-to-zod";
 
+// Example usage:
+// node --env-file=.env bin/generate-schema.mjs "node--article" "nodeArticleWithImage" "include=field_image"
+
 // TODO - convert to TS
-// Abstract this to be a more generic generate schema function
-// Or perhaps a script that generates all schemas from the index
+// Create a script that generates schema from an array of endpoints in a config file
 
 const zodImport = "import { z } from 'astro:content';\n\nexport ";
 
@@ -24,13 +26,18 @@ const writeSchema = (schema, fileName) => {
   });
 };
 
-console.log("Generating schemas...");
+console.log("Generating schema...");
 
-// Generate a schema for an article collection.
-const collection = await client.getCollection("node--page");
+// TODO: Re-implement with commander for args?
+const resourceType = process.argv[2];
+const schemaName = process.argv[3];
+const queryString = process.argv[4];
+
+// Get a collection response to generate a schema from.
+const collection = await client.getCollection(resourceType, { queryString });
 
 const schema = jsonToZod(collection).replace(
   "const schema",
-  "const nodePageSchema"
+  `const ${schemaName}Schema`
 );
-writeSchema(schema, "src/lib/schemas/nodePageSchema.ts");
+writeSchema(schema, `src/lib/schemas/${schemaName}Schema.ts`);
