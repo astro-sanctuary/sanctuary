40644416d6be29cdf0f04a1cad854e754ccc610f
Brian Perry <contact.brianperry@gmail.com>
2024-06-24 08:16:46 -0500

    Remove blog-ts template

--- scripts/init-drupal.sh
+++ scripts/init-drupal.sh
@@ -19,18 +19,11 @@ ddev composer config allow-plugins.ewcomposer/unpack true -n
 ddev composer config repositories.unpack vcs https://gitlab.ewdev.ca/yonas.legesse/drupal-recipe-unpack.git
 ddev composer config repositories.recipe path web/recipes/sanctuary_core
 
-# Open up CORS for local development
-# cat ../scripts/config/enable-local-settings.php >> web/sites/default/settings.php
-# cp web/sites/example.settings.local.php web/sites/default/settings.local.php
-# cp ../scripts/config/development.services.yml web/sites/development.services.yml
-# mkdir config
-# cp ../scripts/config/graphql_compose.settings.yml config/graphql_compose.settings.yml
-
 # Add recipies
 cp -a ../recipes/. web/recipes
 
 # Add useful composer dependencies
-ddev composer require drush/drush drupal/devel drupal/default_content drupal/sanctuary_core ewcomposer/unpack:dev-master
+ddev composer require drush/drush drupal/default_content drupal/sanctuary_core ewcomposer/unpack:dev-master
 
 # Install Drupal
 ddev drush site:install --account-name=admin --account-pass=admin -y
@@ -38,26 +31,6 @@ ddev drush site:install --account-name=admin --account-pass=admin -y
 # Apply recipe
 ddev exec -d /var/www/html/web php core/scripts/drupal recipe recipes/sanctuary_core
 ddev composer unpack drupal/sanctuary_core
-
-### Below should be handled by recipes ###
-
-# Add useful composer dependencies
-# ddev composer require drush/drush drupal/decoupled_router drupal/jsonapi_extras drupal/simple_oauth:^5.0 drupal/graphql_compose drupal/devel
-
-# Install Drupal
-# ddev drush site:install --account-name=admin --account-pass=admin -y
-# ddev drush site:install demo_umami --account-name=admin --account-pass=admin -y
-# ddev drush en jsonapi decoupled_router basic_auth jsonapi_extras simple_oauth graphql_compose graphql_compose_edges devel_generate -y
-
-# Configure JSON:API to allow CRUD operations
-# ddev drush config:set jsonapi.settings read_only 0 -y
-
-# Enable some amount of GraphQL schema
-# ddev drush cim --partial --source=../config -y
-
-# Generate some content
-# ddev drush en devel_generate -y
-# ddev drush genc 25 0
 
 # use the one-time link (CTRL/CMD + Click) from the command below to edit your admin account details.
 ddev drush uli
