64fe65c1be718c62caf4ee4934d7a4b87cabbbfa
Brian Perry <contact.brianperry@gmail.com>
2024-06-26 09:02:44 -0500

    Add caching for client

--- templates/core/src/lib/client.ts
+++ templates/core/src/lib/client.ts
@@ -1,6 +1,8 @@
-import { JsonApiClient } from "@drupal-api-client/json-api-client";
+import { JsonApiClient, createCache } from "@drupal-api-client/json-api-client";
 import { Jsona } from "jsona";
 
 export const client = new JsonApiClient(import.meta.env.DRUPAL_BASE_URL, {
   serializer: new Jsona(),
+  cache: createCache(),
+  debug: import.meta.env.MODE === "development",
 });
