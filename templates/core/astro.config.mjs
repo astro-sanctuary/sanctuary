a7d7839ef34d4056a92d375f87999cf054bba51f
Brian Perry <contact.brianperry@gmail.com>
2024-07-25 10:26:30 -0500

    Optimize images using Astro image

--- templates/core/astro.config.mjs
+++ templates/core/astro.config.mjs
@@ -1,11 +1,17 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
+const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), icon()]
+  integrations: [mdx(), sitemap(), icon()],
+  image: {
+    domains: [IMAGE_DOMAIN],
+  },
 });
