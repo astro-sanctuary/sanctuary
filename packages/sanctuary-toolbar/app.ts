74f2cac494f8d0c02a2e3e79a8b2c736ec947d26
Brian Perry <contact.brianperry@gmail.com>
2024-07-28 10:48:49 -0500

    Initial POC for passing edit link to plugin

--- packages/sanctuary-toolbar/app.ts
+++ packages/sanctuary-toolbar/app.ts
@@ -1,31 +1,19 @@
 import { defineToolbarApp } from "astro/toolbar";
-
-const motivationalMessages = [
-  "You're doing great!",
-  "Keep up the good work!",
-  "You're awesome!",
-  "You're a star!",
-];
+import { render } from "preact";
+import { html } from "htm/preact";
 
 export default defineToolbarApp({
-  init(canvas, app) {
-    const myWindow = document.createElement("astro-dev-toolbar-window");
-    const h1 = document.createElement("h1");
-    h1.textContent =
-      motivationalMessages[
-        Math.floor(Math.random() * motivationalMessages.length)
-      ];
-
-    myWindow.appendChild(h1);
-    canvas.append(myWindow);
-
-    // Display a random message when the app is toggled
-    app.onToggled(({ state }) => {
-      const newMessage =
-        motivationalMessages[
-          Math.floor(Math.random() * motivationalMessages.length)
-        ];
-      h1.textContent = newMessage;
-    });
+  init(canvas) {
+    const content =
+      document.querySelectorAll<HTMLElement>("[data-drupal-edit]");
+    const contentList = Array.from(content);
+    render(
+      html`<astro-dev-toolbar-window
+        >${contentList.map((element) => {
+          return html`<a href=${element.getAttribute("data-drupal-edit") || ""} target="_blank">${element.getAttribute("data-drupal-edit")}</p>`;
+        })}</astro-dev-toolbar-window
+      >`,
+      canvas
+    );
   },
 });
