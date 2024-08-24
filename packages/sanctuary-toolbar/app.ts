805980f1ee6639587ab9c7a96b789cf294922e2d
Brian Perry <contact.brianperry@gmail.com>
2024-08-24 10:25:37 -0500

    toolbar: revisions to elememnt highlighting

--- packages/sanctuary-toolbar/app.ts
+++ packages/sanctuary-toolbar/app.ts
@@ -2,9 +2,7 @@ import { defineToolbarApp } from "astro/toolbar";
 import { render } from "preact";
 import { html } from "htm/preact";
 
-// Re-calculate highlights on resize.
-// Should we do something similar for mutationObserver?
-const resizeObserver = new ResizeObserver(() => {
+const resetPosition = () => {
   const content = document.querySelectorAll<HTMLElement>("[data-sanctuary]");
   const highlights = document.querySelectorAll<HTMLElement>(
     ".sanctuary-highlight",
@@ -13,66 +11,89 @@ const resizeObserver = new ResizeObserver(() => {
     const rect = content[i].getBoundingClientRect();
     highlights[i].style.width = `${rect.width}px`;
     highlights[i].style.height = `${rect.height}px`;
-    highlights[i].style.top = `${rect.top}px`;
-    highlights[i].style.left = `${rect.left}px`;
+    highlights[i].style.top = `${rect.top + window.scrollY}px`;
+    highlights[i].style.left = `${rect.left + window.scrollX}px`;
   }
+};
+
+// Re-calculate highlights on resize.
+// TODO - need to do more work here for routing.
+// Should we do something similar for mutationObserver?
+const resizeObserver = new ResizeObserver(() => {
+  resetPosition();
 });
 
 export default defineToolbarApp({
   init(canvas, app) {
-    const windowStyle = `left: initial;
-      top: 8px;
-      right: 8px;
-      transform: none;
-      width: 350px;
-      min-height: 350px;
-      max-height: 420px;
-      overflow: hidden;`;
-
     const content = document.querySelectorAll<HTMLElement>("[data-sanctuary]");
 
+    const postMessage = (e) => {
+      // Send a generic post message to the parent window.
+      e.preventDefault();
+      if (window && window.top !== window.self) {
+        // TODO  - update second argument to be target origin.
+        window.parent.postMessage({ type: "SANCTUARY_POST_MESSAGE" }, "*");
+      }
+    };
+
     render(
-      html`<astro-dev-toolbar-window style=${windowStyle}>
-        <h2>Sanctuary Toolbar</h2>
-        ${Array.from(content, (element) => {
-          // highlight the element
+      html`
+        ${Array.from(content, (element, index) => {
+          // Place a button and highlight for the element
+          element.setAttribute("data-index", index.toString());
+          element.addEventListener("mouseover", () => {
+            const relatedHighlight = document.querySelector(
+              `.sanctuary-highlight[data-index="${index}"]`,
+            );
+            if (relatedHighlight) {
+              (relatedHighlight as HTMLElement).style.display = "block";
+            }
+          });
           const rect = element.getBoundingClientRect();
-          const style = `top: ${rect.top}px;
-            left: ${rect.left}px;
+          const buttonStyle = `top: ${rect.top + window.scrollY}px;
+            left: ${rect.left + window.scrollX}px;
+            z-index: 2000000001;
+            position: absolute;`;
+          const iconStyle = `width: 1.5rem; height: 1.5rem; margin-left: 0;`;
+          const highlightStyle = `top: ${rect.top + window.scrollY}px;
+            left: ${rect.left + window.scrollX}px;
             width: ${rect.width}px;
             height: ${rect.height}px;
             display: none;`;
 
           render(
-            html`<astro-dev-toolbar-highlight
-              class="sanctuary-highlight"
-              style=${style}
-            />`,
+            html`
+              <astro-dev-toolbar-button
+                class="sanctuary-button"
+                href="https://sanctuary.ddev.site/node/2/edit"
+                onClick=${postMessage}
+                style=${buttonStyle}
+              >
+                <astro-dev-toolbar-icon
+                  icon="gear"
+                  style=${iconStyle}
+                  data-index=${index}
+                ></astro-dev-toolbar-icon>
+              </astro-dev-toolbar-button>
+              <astro-dev-toolbar-highlight
+                class="sanctuary-highlight"
+                style=${highlightStyle}
+                onMouseOut=${(e) => (e.target.style.display = "none")}
+                data-index=${index}
+              ></astro-dev-toolbar-highlight>
+            `,
             document.body,
           );
-          // List an entry for the element in the toolbar window.
-          const props = JSON.parse(
-            element.getAttribute("data-sanctuary") || "",
-          );
-          // TODO - try to add icon
-          return html`<astro-dev-toolbar-card link=${props.edit}>
-            ${props.title}
-          </astro-dev-toolbar-card>`;
         })}
-      </astro-dev-toolbar-window>`,
+      `,
       canvas,
     );
 
     // Set up and tear down highlighted elements when the toolbar is toggled.
     app.onToggled(({ state }) => {
-      const highlights = document.querySelectorAll<HTMLElement>(
-        ".sanctuary-highlight",
-      );
-      highlights.forEach((highlight) => {
-        highlight.style.display = state ? "block" : "none";
-      });
       if (state) {
         resizeObserver.observe(document.body);
+        resetPosition();
       } else {
         resizeObserver.unobserve(document.body);
       }
