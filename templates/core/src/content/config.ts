eae8e7ad5a6b8f789017eac8d801dc46f18849b8
Brian Perry <contact.brianperry@gmail.com>
2024-09-24 16:05:49 +0200

    Convert slug template to use content collections

--- templates/core/src/content/config.ts
+++ templates/core/src/content/config.ts
@@ -1,8 +1,21 @@
 import { defineCollection } from "astro:content";
 import { client } from "../../client";
+import { nodePageSchema } from "@lib/schemas/nodePageSchema";
 import { nodeArticleWithImageSchema } from "@/lib/schemas/nodeArticleWithImageSchema.ts";
 
-const articles = defineCollection({
+const page = defineCollection({
+  loader: async () => {
+    const pages: any = await client.getCollection("node--page");
+    return pages.map((page: any) => ({
+      id: page.id,
+      ...page,
+    }));
+  },
+  // TODO - is there a way to derive the schema dynamically?
+  schema: nodePageSchema.element,
+});
+
+const article = defineCollection({
   loader: async () => {
     const articles: any = await client.getCollection("node--article", {
       queryString: "include=field_image",
@@ -16,4 +29,4 @@ const articles = defineCollection({
   schema: nodeArticleWithImageSchema.element,
 });
 
-export const collections = { articles };
+export const collections = { page, article };
