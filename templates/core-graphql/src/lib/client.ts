62e3afc4d1f900047eea291525479ccd689bc6d7
Brian Perry <contact.brianperry@gmail.com>
2024-12-07 10:13:22 -0600

    intiial example query for main menu

--- templates/core-graphql/src/lib/client.ts
+++ templates/core-graphql/src/lib/client.ts
@@ -1,4 +1,5 @@
 import { JsonApiClient, createCache } from "@drupal-api-client/json-api-client";
+import { GraphqlClient } from "@drupal-api-client/graphql-client";
 import { DecoupledRouterClient } from "@drupal-api-client/decoupled-router-client";
 import { Jsona } from "jsona";
 
@@ -7,6 +8,20 @@ export const client = new JsonApiClient(
   {
     serializer: new Jsona(),
     cache: import.meta.env.MODE === "development" ? undefined : createCache(),
+    debug: import.meta.env.MODE === "development",
+    authentication: {
+      type: "OAuth",
+      credentials: {
+        clientId: import.meta.env.CLIENT_ID,
+        clientSecret: import.meta.env.CLIENT_SECRET,
+      },
+    },
+  },
+);
+
+export const graphqlClient = new GraphqlClient(
+  import.meta.env.PUBLIC_DRUPAL_BASE_URL,
+  {
     debug: import.meta.env.MODE === "development",
     authentication: {
       type: "OAuth",
