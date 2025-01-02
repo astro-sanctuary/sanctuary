41a499db48d3faf34460eb77b5f25b4407c22037
Brian Perry <contact.brianperry@gmail.com>
2025-01-02 10:43:03 -0600

    Add missing module file

--- modules/sanctuary/modules/sanctuary_graphql/src/Plugin/GraphQLCompose/FieldType/IconifyIcon.php
+++ modules/sanctuary/modules/sanctuary_graphql/src/Plugin/GraphQLCompose/FieldType/IconifyIcon.php
@@ -8,15 +8,24 @@ use Drupal\graphql_compose\Plugin\GraphQL\DataProducer\FieldProducerTrait;
 use Drupal\graphql_compose\Plugin\GraphQLCompose\GraphQLComposeFieldTypeBase;
 
 /**
- * {@inheritdoc}
+ * {@inheritDoc}
  *
  * @GraphQLComposeFieldType(
  *   id = "iconify_icon",
- *   type_sdl = "IconifyIcon",
+ *   type_sdl = "String",
  * )
  */
 class IconifyIcon extends GraphQLComposeFieldTypeBase {
 
   use FieldProducerTrait;
 
+  /**
+   * Value to return to getProducerProperty in FieldProducerTrait.
+   *
+   * This could be value, entity, something_id, whatever.
+   * It's equivalent to $entity->field_abc->value
+   *
+   * @var string
+   */
+  public $producerProperty = 'icon';
 }
