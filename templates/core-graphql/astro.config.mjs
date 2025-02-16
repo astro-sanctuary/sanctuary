f391cb830b493e23a4e678d13e92062da27c6f71
Brian Perry <contact.brianperry@gmail.com>
2025-02-16 10:15:41 -0600

    Rename toolbar app to toolbar-drupal

--- templates/core-graphql/astro.config.mjs
+++ templates/core-graphql/astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import node from "@astrojs/node";
-import sanctuaryToolbar from "sanctuary-toolbar";
+import toolbarDrupal from "@astro-sanctuary/toolbar-drupal";
 
 const { IMAGE_DOMAIN, NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(
   process.env.NODE_ENV,
@@ -16,7 +16,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), icon(), sanctuaryToolbar],
+  integrations: [mdx(), sitemap(), icon(), toolbarDrupal],
   image: {
     domains: [IMAGE_DOMAIN],
   },
